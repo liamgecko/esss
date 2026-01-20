71292215ccc6b64aad432b849be620cc85348e8f
Liam Young <liam@geckoengage.com>
2026-01-20 11:47:17 +0000

    Use HTTP WPGraphQL endpoint fallback
    - Document temporary HTTP endpoint for cms subdomain due to TLS errors
    - Allow http/https uploads domain for Next image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
+        protocol: "http",
+        hostname: "cms.engineeringspecialisedsupport.com",
+        pathname: "/wp-content/uploads/**",
+      },
+      {
         protocol: "https",
         hostname: "cms.engineeringspecialisedsupport.com",
         pathname: "/wp-content/uploads/**",
